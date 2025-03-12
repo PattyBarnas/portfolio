7c2531bf3e96f1fb3cffeeb6ce7137db2f27c01c
Patryk Barnas <patrykbarnas@yahoo.com>
2025-03-12 09:29:44 -0500

    Updates

---
+++ precache-manifest.ee5e0aa555982dc66b74a98031db3294.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0510caf61d0d30fb5d1f97f19e06702f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7765570b1d175c08b7e",
+    "url": "/static/css/main.3ed68377.chunk.css"
+  },
+  {
+    "revision": "06591a74dca1acd49795",
+    "url": "/static/js/2.484a4363.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.484a4363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7765570b1d175c08b7e",
+    "url": "/static/js/main.c18323eb.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  },
+  {
+    "revision": "d95f2cf93d54644d684496d1cd067754",
+    "url": "/static/media/ClothingInc.d95f2cf9.jpg"
+  },
+  {
+    "revision": "880b0b00f80f1217b26e3f1b2d523a89",
+    "url": "/static/media/FiretekHVAC.880b0b00.png"
+  },
+  {
+    "revision": "e6eef709c779e670a034ae3122427574",
+    "url": "/static/media/PB.e6eef709.pdf"
+  },
+  {
+    "revision": "d95aa07a7ea90c17cd1f27501d296d24",
+    "url": "/static/media/Portfolio-shop.d95aa07a.png"
+  },
+  {
+    "revision": "4834bcc03a6791778cb7fa7b5a9eae38",
+    "url": "/static/media/me.4834bcc0.jpg"
+  },
+  {
+    "revision": "9434ffa9383b75b5089bbbc1064e0fa0",
+    "url": "/static/media/phone-men.9434ffa9.svg"
+  },
+  {
+    "revision": "5ba5c0ecbddbb4a7e5ba0985054bebe6",
+    "url": "/static/media/vid.5ba5c0ec.mp4"
+  }
+]);
