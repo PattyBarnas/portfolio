c5a6ec641bb6e9b420393188c079f7443110d254
Patryk Barnas <patrykbarnas@yahoo.com>
2026-09-20 08:36:28 -0500

    final mobile responsiveness touches

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,7 +18,7 @@ const About = () => {
         const entry = entries[0];
         entry.isIntersecting && setVisible(true);
       },
-      { threshold: 0.2 },
+      { threshold: 0.05 },
     );
     observer.observe(ref.current);
   }, []);
